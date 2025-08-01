5eefd94d1339e579fb098d2e1c0c00136f260e74
guiuferrerm <208443537+guiuferrerm@users.noreply.github.com>
2025-08-01 17:48:05 +0200

    Update __init__.py

--- src/xemapytools/__init__.py
+++ src/xemapytools/__init__.py
@@ -1,6 +1,4 @@
-"""
-xemapytools
+# src/xemapytools/__init__.py
 
-Utilities around retrieving and standardizing XEMA meteorological data.
-"""
-# Intentionally no re-exports; users must import from submodules explicitly.
+# It's good practice to import __version__ here
+__version__ = "0.1.0" # Make sure this matches your pyproject.toml
